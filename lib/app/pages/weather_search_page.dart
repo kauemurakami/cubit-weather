65cbfd11c4636144da606890fa2394f9839226c3
kauemurakami <kauetmurakami@gmail.com>
2020-08-20 12:04:21 -0300

    criando cubit and events

--- lib/app/pages/weather_search_page.dart
+++ lib/app/pages/weather_search_page.dart
@@ -5,41 +5,9 @@ class WeatherSearchPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Weather Search"),
-      ),
-      body: Container(
-        padding: EdgeInsets.symmetric(vertical: 16),
-        alignment: Alignment.center,
-        child: BlocBuilder<WeatherCubit, WeatherState>(
-          builder: (context, state) {
-            if (state is WeatherInitial) {
-              return buildInitialInput();
-            } else if (state is WeatherLoading) {
-              return buildLoading();
-            } else if (state is WeatherLoaded) {
-              return buildColumnWithData(state.weather);
-            } else {
-              // (state is WeatherError)
-              return buildInitialInput();
-            }
-          },
+        appBar: AppBar(
+          title: Text("Weather Search"),
         ),
-      ),
-    );
+        body: Container());
   }
-  BlocConsumer<WeatherCubit, WeatherState>(
-  listener: (context, state) {
-    if (state is WeatherError) {
-      Scaffold.of(context).showSnackBar(
-        SnackBar(
-          content: Text(state.message),
-        ),
-      );
-    }
-  },
-  builder: (context, state) {
-    // Previously written code here...
-  },
-),
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
+import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather_cubit/app/pages/weather_search_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,10 +9,35 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
-      home: BlocProvider(
-        create: (context) => WeatherCubit(FakeWeatherRepository()),
-        child: WeatherSearchPage(),
-      ),
+      home: WeatherSearchPage(),
     );
   }
 }
+/*
+### CHANGE NOTIFIER ###
+class MyChangeNotifier extends ChangeNotifier {
+  int field1;
+  String field2;
+
+  voidChangeState() {
+    field2 = 'New Value';
+    notifyListeners();
+  }
+}
+
+#### CUBIT ####
+class MyState {
+  final int field1;
+  final String field2;
+
+  MyState(this.field1, this.field2);
+}
+
+class MyCubit extends Cubit<MyState> {
+  MyCubit() : super(MyState(0, 'InitialValue'));
+
+  void changeState() {
+    emit(MyState(0, 'newValue'));
+  }
+}
+*/
